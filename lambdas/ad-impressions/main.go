8b6b323f2a72fb57477dd791437c099c9d2e8108
Jonathan Bryant <watersofmemory@gmail.com>
2019-05-15 20:10:00 -0600

    Starting application code, built stub Lambda.

---
+++ lambdas/ad-impressions/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"os"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+var encoder = json.NewEncoder(os.Stdout)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, v interface{}) error {
+	log.Printf("Ad Impressions Output")
+	if err := encoder.Encode(v); err != nil {
+		log.Printf("Error encoding request: %s", err)
+	}
+	return nil
+}
