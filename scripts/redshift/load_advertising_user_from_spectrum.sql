cfa6bf31607782e3140028898213d31c168cf4c8
Jonathan Bryant <watersofmemory@gmail.com>
2019-05-24 13:19:00 -0600

    Using ? in scripts instead of $n

--- scripts/redshift/load_advertising_user_from_spectrum.sql
+++ scripts/redshift/load_advertising_user_from_spectrum.sql
@@ -7,7 +7,7 @@ SELECT
 FROM
   warehouse.advertising
 WHERE
-      year = $1
-  AND year = $2
-  AND impression_at BETWEEN $3 AND $4
+      year = ?
+  AND year = ?
+  AND impression_at BETWEEN ? AND ?
 ;
