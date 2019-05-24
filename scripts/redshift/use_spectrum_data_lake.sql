5b1f6b7f1ebc5d6bdd4950d6a20bf12756442517
Jonathan Bryant <watersofmemory@gmail.com>
2019-05-24 16:21:14 -0600

    Spectrum permissions

--- scripts/redshift/use_spectrum_data_lake.sql
+++ scripts/redshift/use_spectrum_data_lake.sql
@@ -4,11 +4,3 @@ DATABASE ?
 IAM_ROLE ?
 CREATE EXTERNAL DATABASE
 IF NOT EXISTS;
-
-CREATE EXTERNAL TABLE warehouse.advertising (
-  session_id    VARCHAR(64) NOT NULL,
-  user_id       VARCHAR(64) NOT NULL,
-  ad_id         VARCHAR(64) NOT NULL,
-  impression_at TIMESTAMP   NOT NULL,
-  click_at      TIMESTAMP
-);
