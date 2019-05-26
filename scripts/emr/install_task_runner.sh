e2616bd3194eac2ac78826306941645d19f87ab3
Jonathan Bryant <watersofmemory@gmail.com>
2019-05-25 18:12:02 -0600

    Refactored build and template to take AWS credentials for task runner and to inject critical config properties into config.json

--- scripts/emr/install_task_runner.sh
+++ scripts/emr/install_task_runner.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+# Make everything verbose and exit on command failure
 set -e -x
 
+# Download the TaskRunner
 aws s3 cp "s3://datapipeline-us-east-1/us-east-1/software/latest/TaskRunner/TaskRunner-1.0.jar" "${HOME}/TaskRunner-1.0.jar"
-nohup java -jar "${HOME}/TaskRunner-1.0.jar" --workerGroup="${1}" --region="${2}" --logUri="${3}" &
+
+# Create a configuration file with the credentials
+echo "{\"access-id\": \"${1}\", \"private-key\": \"${2}\"}" | jq -cr '.' > "${HOME}/credentials.json"
+
+# Start the Task runner in the background
+nohup java -jar "${HOME}/TaskRunner-1.0.jar" --config="${HOME}/credentials.json" --workerGroup="${3}" --region="${4}" --logUri="${5}" &
