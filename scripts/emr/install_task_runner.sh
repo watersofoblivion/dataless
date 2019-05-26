e6a0ae2b62e6c86250864354ab7dd77157874f0a
Jonathan Bryant <watersofmemory@gmail.com>
2019-05-25 18:15:26 -0600

    Adding task runner setup to instance as well

--- scripts/emr/install_task_runner.sh
+++ scripts/emr/install_task_runner.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-# Make everything verbose and exit on command failure
 set -e -x
 
-# Download the TaskRunner
+# Install Task Runner
 aws s3 cp "s3://datapipeline-us-east-1/us-east-1/software/latest/TaskRunner/TaskRunner-1.0.jar" "${HOME}/TaskRunner-1.0.jar"
-
-# Create a configuration file with the credentials
 echo "{\"access-id\": \"${1}\", \"private-key\": \"${2}\"}" | jq -cr '.' > "${HOME}/credentials.json"
-
-# Start the Task runner in the background
 nohup java -jar "${HOME}/TaskRunner-1.0.jar" --config="${HOME}/credentials.json" --workerGroup="${3}" --region="${4}" --logUri="${5}" &
