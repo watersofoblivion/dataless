c1f73e6addc05926dffa4a721be4c623633141ed
Jonathan Bryant <watersofmemory@gmail.com>
2019-06-11 18:11:42 -0600

    Adding enable/disable pipeline flag, fixing script to handle a delta.

--- svc/advertising/scripts/shell/change_write_capacity.sh
+++ svc/advertising/scripts/shell/change_write_capacity.sh
@@ -2,29 +2,40 @@
 
 # Changes the provisioned write capacity units on a DynamoDB table, leaving the
 # provisioned read capacity unchanged.
+#
+# Takes two arguments: the table to change, and the delta to change the write
+# capacity by.
 
-TABLE_NAME=""
-WRITE_CAPACITY_UNITS=""
+TABLE_NAME="${1}"
+WRITE_CAPACITY_CHANGE="${2}"
 
-# Get current read capacity
-READ_CAPACITY_UNITS=$(aws dynamodb describe-table --table-name "${TABLE_NAME}" --query "Table.ProvisionedThroughput.ReadCapacityUnits")
+# Get current capacity
+READ_CAPACITY_UNITS=$(aws dynamodb describe-table --table-name "${TABLE_NAME}" --query "Table.ProvisionedThroughput.ReadCapacityUnits" --output text)
+WRITE_CAPACITY_UNITS=$(aws dynamodb describe-table --table-name "${TABLE_NAME}" --query "Table.ProvisionedThroughput.WriteCapacityUnits" --output text)
+
+# Compute new write capacity.  Ensure we don't go below 1.
+WRITE_CAPACITY_UNITS=$((${WRITE_CAPACITY_UNITS} + ${WRITE_CAPACITY_CHANGE}))
+if [[ ${WRITE_CAPACITY_UNITS} < 1 ]]; then
+  WRITE_CAPACITY_UNITS=1
+fi
 
 # Update write capacity
 aws dynamodb update-table --table-name "${TABLE_NAME}" --provisioned-throughput ReadCapacityUnits=${READ_CAPACITY_UNITS},WriteCapacityUnits=${WRITE_CAPACITY_UNITS}
 
 # Wait for update to complete
-for /bin/true; do
+while true; do
   echo -n "Polling table ${TABLE_NAME}: "
-  TABLE_STATUS=$(aws dynamodb describe-table --table-name "${TABLE_NAME}" --query "Table.TableStatus")
+  TABLE_STATUS=$(aws dynamodb describe-table --table-name "${TABLE_NAME}" --query "Table.TableStatus" --output text)
   echo "${TABLE_STATUS}"
-  
+
   case "${TABLE_STATUS}" in
     "UPDATING")
       sleep 1
       ;;
 
     "ACTIVE")
-      break
+      echo "Table updated"
+      exit 0
       ;;
 
     *)
